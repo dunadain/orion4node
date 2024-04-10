b864052c3aea5ee6923abb41680f8791acbe699d
Ryan Sampras <maelendil@gmail.com>
2024-04-10 16:24:35 +0800

    Update package-and-msg.test.ts

--- tests/protocol/package-and-msg.test.ts
+++ tests/protocol/package-and-msg.test.ts
@@ -15,16 +15,27 @@ describe('Package Protocol', () => {
                 4, 5, 2, 1, 3, 5, 2, 5
             ]
         };
-        const str = JSON.stringify(obj);
-        const buffer = pack.encode(pack.PackType.DATA, Buffer.from(str, 'utf8'));
-        const data = pack.decode(buffer);
-        expect(data[0].type).toBe(pack.PackType.DATA);
-        expect(data[0].body?.toString()).toBe(str);
+        let str = JSON.stringify(obj);
+        let buffer = pack.encode(pack.PackType.DATA, Buffer.from(str, 'utf8'));
+        let decodedData = pack.decode(buffer);
+        expect(decodedData[0].type).toBe(pack.PackType.DATA);
+        expect(decodedData[0].body?.toString()).toBe(str);
+
         const hbBuffer = pack.encode(pack.PackType.HEARTBEAT);
         expect(hbBuffer.length).toBe(4);
         const hbData = pack.decode(hbBuffer);
         expect(hbData[0].type).toBe(pack.PackType.HEARTBEAT);
         expect(hbData[0].body).toBeUndefined();
+
+        const originalErr = { code: 500, msg: 'some error' };
+        str = JSON.stringify(originalErr);
+        buffer = pack.encode(pack.PackType.ERROR, Buffer.from(str));
+        decodedData = pack.decode(buffer);
+        expect(decodedData[0].type).toBe(pack.PackType.ERROR);
+        expect(decodedData[0].body?.toString()).toBe(str);
+        const errObj = JSON.parse(String(decodedData[0].body?.toString())) as { code: number, msg: string };
+        expect(errObj.code).toBe(originalErr.code);
+        expect(errObj.msg).toBe(originalErr.msg);
     });
 
     it('should not change data content through message encode and decode', () => {
