2c7854dd4a8574da611a78932a3a325530a2bcd1
Ryan Sampras <maelendil@gmail.com>
2024-04-11 17:52:28 +0800

    Update UWebSocketTransport.ts

--- src/transport/uws/UWebSocketTransport.ts
+++ src/transport/uws/UWebSocketTransport.ts
@@ -10,6 +10,7 @@ export class UWebSocketTransport extends Component {
     private _clientMgr: ClientManager | undefined;
     async start() {
         const app = App();
+        const host = this.server.addr ? this.server.addr : 'localhost';
         app.ws('/*', {
             sendPingsAutomatically: false,
             idleTimeout: 0,
@@ -32,8 +33,9 @@ export class UWebSocketTransport extends Component {
                 client?.dispose();
                 this.clientMgr.removeClient(ws);
             },
-        }).listen(this.server.port, token => {
-            if (token) logger.info(`Listening on port: ${String(this.server.port)}`);
+        }).listen(host, this.server.port, (token) => {
+            if (token)
+                logger.info(`Listening on ${host}:${String(this.server.port)}`);
             else logger.error(`Server ${this.server.addr} failed to listen`);
         });
     }
@@ -41,7 +43,8 @@ export class UWebSocketTransport extends Component {
     get clientMgr() {
         if (!this._clientMgr) {
             this._clientMgr = this.getComponent(ClientManager);
-            if (!this._clientMgr) throw new Error('ClientManager Component is required!');
+            if (!this._clientMgr)
+                throw new Error('ClientManager Component is required!');
         }
         return this._clientMgr;
     }
