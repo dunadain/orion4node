8bf6be5a6328ca2096e4c3dea4f66604fde2365e
Ryan Sampras <maelendil@gmail.com>
2024-04-11 10:39:41 +0800

    Create IpUtil.ts

---
+++ src/utils/IpUtil.ts
@@ -0,0 +1,17 @@
+import * as os from 'node:os';
+class IpUtil {
+    address() {
+        const networkInterfaces = os.networkInterfaces();
+        const addresses: string[] = [];
+
+        Object.values(networkInterfaces).forEach((netInterface) => {
+            netInterface?.filter((prop) => prop.family === 'IPv4' && !prop.internal)
+                .forEach((address) => {
+                    addresses.push(address.address);
+                });
+        });
+        return addresses[0];
+    }
+}
+
+export const ipUtil = new IpUtil();
