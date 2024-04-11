2f440dbc51006ca690dac18e50463b4a804ff729
Ryan Sampras <maelendil@gmail.com>
2024-04-11 18:04:08 +0800

    Update HandShake.ts

--- src/transport/handlers/HandShake.ts
+++ src/transport/handlers/HandShake.ts
@@ -5,47 +5,56 @@ import { ClientState, SocketClient } from '../SocketClient';
 import { PackType, encode } from '../protocol/Package';
 import { PkgHandler } from './PkgHandler';
 interface HandShakeInfo {
-    sys: {
-        ver: string;
-        protoVer?: number;
-    } | undefined;
+    sys: { ver: string; protoVer?: number } | undefined;
     user: unknown;
 }
 export class HandShake implements PkgHandler {
-    constructor(private client: SocketClient<unknown>) { }
+    constructor(private client: SocketClient<unknown>) {}
     checkClient?: ((ver: string) => boolean) | undefined; // can be injected through prototype
     handle(msg: Buffer): void {
         if (ClientState.Default !== this.client.state) return;
-
 
         let data: HandShakeInfo;
         try {
             data = JSON.parse(msg.toString()) as HandShakeInfo;
         } catch (e) {
-            this.processError(ErrorCode.InvaildHandShakeInfo, 'What went wrong:handshake info json parse');
-            logger.error(`handshake info json parse error. received msg was ${msg.toString()}`);
+            this.processError(
+                ErrorCode.InvaildHandShakeInfo,
+                'What went wrong:handshake info json parse'
+            );
+            logger.error(
+                `handshake info json parse error. received msg was ${msg.toString()}`
+            );
             return;
         }
 
         if (!data.sys) {
-            this.processError(ErrorCode.InvaildHandShakeInfo, 'Handshake info is insufficient.');
+            this.processError(
+                ErrorCode.InvaildHandShakeInfo,
+                'Handshake info is insufficient.'
+            );
             return;
         }
         if (typeof this.checkClient === 'function') {
             if (!data.sys.ver || !this.checkClient(data.sys.ver)) {
-                this.processError(ErrorCode.OutdatedClient, 'The client is outdated');
+                this.processError(
+                    ErrorCode.OutdatedClient,
+                    'The client is outdated'
+                );
                 return;
             }
         }
 
         const sysRsp = {
-            heartbeat: netConfig.hearbeatInterval
+            heartbeat: netConfig.hearbeatInterval,
         };
 
         const res = {
-            sys: sysRsp
+            sys: sysRsp,
         };
-        this.client.sendBuffer(encode(PackType.HANDSHAKE, Buffer.from(JSON.stringify(res))));
+        this.client.sendBuffer(
+            encode(PackType.HANDSHAKE, Buffer.from(JSON.stringify(res)))
+        );
         this.client.state = ClientState.WaitForAck;
     }
 
