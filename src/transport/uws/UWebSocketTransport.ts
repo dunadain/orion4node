1a8b40c2804356293b2441c435307fcd9e9160cd
Ryan Sampras <maelendil@gmail.com>
2024-04-11 18:21:17 +0800

    Update UWebSocketTransport.ts

--- src/transport/uws/UWebSocketTransport.ts
+++ src/transport/uws/UWebSocketTransport.ts
@@ -1,43 +1,54 @@
-import { App, DISABLED } from 'uWebSockets.js';
+import { App, DISABLED, TemplatedApp } from 'uWebSockets.js';
 import { Component } from '../../component/Component';
 import { logger } from '../../logger/Logger';
 import { ClientManager } from '../../component/ClientManager';
 import { UWebSocketClient } from './UWebSocketClient';
 import { EventEmitter } from 'node:events';
+import { Server } from '../../server/Server';
 
 export class UWebSocketTransport extends Component {
+    app: TemplatedApp;
     readonly eventEmitter = new EventEmitter();
     private _clientMgr: ClientManager | undefined;
+
+    constructor(server: Server) {
+        super(server);
+        this.app = App();
+    }
     async start() {
-        const app = App();
         const host = this.server.addr ? this.server.addr : 'localhost';
-        app.ws('/*', {
-            sendPingsAutomatically: false,
-            idleTimeout: 0,
-            compression: DISABLED,
-            open: (ws) => {
-                const client = new UWebSocketClient(this.eventEmitter);
-                client.socket = ws;
-                client.init();
-                this.clientMgr.addClient(client);
-            },
-            message: (ws, message) => {
-                this.clientMgr.getClient(ws)?.onMessage(message);
-            },
-            drain: (ws) => {
-                const client = this.clientMgr.getClient(ws);
-                client?.onDrain?.call(client);
-            },
-            close: (ws) => {
-                const client = this.clientMgr.getClient(ws);
-                client?.dispose();
-                this.clientMgr.removeClient(ws);
-            },
-        }).listen(host, this.server.port, (token) => {
-            if (token)
-                logger.info(`Listening on ${host}:${String(this.server.port)}`);
-            else logger.error(`Server ${this.server.addr} failed to listen`);
-        });
+        this.app
+            .ws('/*', {
+                sendPingsAutomatically: false,
+                idleTimeout: 0,
+                compression: DISABLED,
+                open: (ws) => {
+                    const client = new UWebSocketClient(this.eventEmitter);
+                    client.socket = ws;
+                    client.init();
+                    this.clientMgr.addClient(client);
+                },
+                message: (ws, message) => {
+                    this.clientMgr.getClient(ws)?.onMessage(message);
+                },
+                drain: (ws) => {
+                    const client = this.clientMgr.getClient(ws);
+                    client?.onDrain?.call(client);
+                },
+                close: (ws) => {
+                    const client = this.clientMgr.getClient(ws);
+                    client?.dispose();
+                    this.clientMgr.removeClient(ws);
+                },
+            })
+            .listen(host, this.server.port, (token) => {
+                if (token)
+                    logger.info(
+                        `Listening on ${host}:${String(this.server.port)}`
+                    );
+                else
+                    logger.error(`Server ${this.server.addr} failed to listen`);
+            });
     }
 
     get clientMgr() {
