103cbe69228cb57eb4f21ca59557a9d468b804c9
Ryan Sampras <maelendil@gmail.com>
2024-04-12 21:36:04 +0800

    Update UWebSocketTransport.ts

--- src/transport/uws/UWebSocketTransport.ts
+++ src/transport/uws/UWebSocketTransport.ts
@@ -59,7 +59,7 @@ export class UWebSocketTransport extends Component {
         if (!this._clientMgr) {
             this._clientMgr = this.getComponent(ClientManager);
             if (!this._clientMgr)
-                throw new Error('ClientManager Component is required!');
+                this._clientMgr = this.server.addComponent(ClientManager);
         }
         return this._clientMgr;
     }
