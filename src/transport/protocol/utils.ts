0decd5647a7a589567f92fc2e3933cabc4c37031
Ryan Sampras <maelendil@gmail.com>
2024-04-18 19:40:51 +0800

    Update utils.ts

--- src/transport/protocol/utils.ts
+++ src/transport/protocol/utils.ts
@@ -1,12 +1,11 @@
 export function copyArray(dest: Buffer, doffset: number, src: Buffer, soffset: number, length: number) {
     if ('function' === typeof src.copy) {
         // Buffer
-        return src.copy(dest, doffset, soffset, soffset + length);
+        src.copy(dest, doffset, soffset, soffset + length);
     } else {
         // Uint8Array
         for (let index = 0; index < length; index++) {
             dest[doffset++] = src[soffset++];
         }
-        return doffset;
     }
 }
