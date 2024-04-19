30cec75028ce28436126481e90c5b21fd37c1433
Ryan Sampras <maelendil@gmail.com>
2024-04-19 16:54:20 +0800

    add handlerLoader

---
+++ src/router/HandlerLoader.ts
@@ -0,0 +1,21 @@
+import { Component } from '../component/Component';
+import * as path from 'node:path';
+import * as fs from 'fs/promises';
+import { logErr } from '../logger/Logger';
+
+export class HandlerLoader extends Component {
+    async init() {
+        if (!require.main) return;
+        const handlerDir = path.join(require.main.path, 'handler');
+        try {
+            const list = await fs.readdir(handlerDir);
+            const promises: Promise<unknown>[] = [];
+            for (const fileName of list) {
+                promises.push(import(path.join(handlerDir, fileName)));
+            }
+            await Promise.all(promises);
+        } catch (e) {
+            logErr(e);
+        }
+    }
+}
