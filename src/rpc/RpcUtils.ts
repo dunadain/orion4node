a0ff9f9a7368d2324f41facc8ec32471890e6e25
Ryan Sampras <maelendil@gmail.com>
2024-04-22 13:42:54 +0800

    load rpc functions

---
+++ src/rpc/RpcUtils.ts
@@ -0,0 +1,10 @@
+const rpcCalls = new Map<string, (param: unknown) => Promise<unknown>>();
+export function addRpcCall(funcName: string, func: (param: unknown) => Promise<unknown>) {
+    rpcCalls.set(funcName, func);
+}
+
+export function callRpc(funcName: string, param: unknown) {
+    const rpcCall = rpcCalls.get(funcName);
+    if (!rpcCall) throw new Error(`rpc call ${funcName} not found`);
+    return rpcCall(param);
+}
--- src/server/FileLoader.ts
+++ src/server/FileLoader.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Component } from '../component/Component';
 import * as path from 'node:path';
 import * as fs from 'fs/promises';
+import { existsSync } from 'node:fs';
+import { addRpcCall } from '../rpc/RpcUtils';
 
 /**
  * load handler files and remote files
@@ -8,11 +12,31 @@ import * as fs from 'fs/promises';
 export class FileLoader extends Component {
     async init() {
         if (!require.main) return;
-        const handlerDir = path.join(require.main.path, 'handler');
-        const list = await fs.readdir(handlerDir);
         const promises: Promise<unknown>[] = [];
-        for (const fileName of list) {
-            promises.push(import(path.join(handlerDir, fileName)));
+        const handlerDir = path.join(require.main.path, 'handler');
+        if (existsSync(handlerDir)) {
+            const list = await fs.readdir(handlerDir);
+            for (const fileName of list) {
+                const filePath = path.join(handlerDir, fileName);
+                promises.push(import(filePath));
+            }
+        }
+        const remoteDir = path.join(require.main.path, 'remote');
+        if (existsSync(remoteDir)) {
+            const list = await fs.readdir(remoteDir);
+            for (const fileName of list) {
+                promises.push(
+                    import(path.join(remoteDir, fileName)).then((m) => {
+                        const prototype = m.default.prototype;
+                        for (const key in prototype) {
+                            if (typeof prototype[key] === 'function') {
+                                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                                addRpcCall(key, prototype[key]);
+                            }
+                        }
+                    })
+                );
+            }
         }
         await Promise.all(promises);
     }
