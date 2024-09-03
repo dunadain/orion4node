9134b9aef4751c34d97242a28da339bbebc5b043
Ryan Sampras <maelendil@gmail.com>
2024-09-03 22:11:05 +0800

    change rpc group name

--- src/rpc/StatelessRpcServer.ts
+++ src/rpc/StatelessRpcServer.ts
@@ -3,6 +3,6 @@ import { RpcServerBase } from './RpcServerBase';
 export class StatelessRpcServer extends RpcServerBase {
 	async init() {
 		this.subject = `rpc.${this.server.serverType}.>`; // example: rpc.{servertype}.{remotehandler}.method.request.response
-		this.opt = { queue: this.server.serverType };
+		this.opt = { queue: 'rpc.' + this.server.serverType };
 	}
 }
