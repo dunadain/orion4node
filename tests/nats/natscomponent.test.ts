6488a9bc08fa72b11dcec16682e14e9923771847
Ryan Sampras <maelendil@gmail.com>
2024-09-16 19:41:25 +0800

    Update natscomponent.test.ts

--- tests/nats/natscomponent.test.ts
+++ tests/nats/natscomponent.test.ts
@@ -5,7 +5,7 @@ import { Server } from '../../src/server/Server.mjs';
 describe('nats component', () => {
     it('should be able to connect to nats', async () => {
         const nats = new NatsComponent({} as Server);
-        Object.getPrototypeOf(nats).getConnectionOption = () => Promise.resolve({ servers: ['nats://localhost:4000'] });
+        process.env.NATS_URL = 'nats://localhost:4000';
         await expect(nats.init()).rejects.toThrowError('CONNECTION_REFUSED');
     });
 });
