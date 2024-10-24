c006dff4adfefd135428b6c25b51973550cbf6a2
Ryan Sampras <maelendil@gmail.com>
2024-10-25 00:54:02 +0800

    update dist

--- dist/src/rpc/RpcClient.d.mts
+++ dist/src/rpc/RpcClient.d.mts
@@ -2,7 +2,7 @@ import { Component } from '../component/Component.mjs';
 import type { Constructor } from '../index.mjs';
 import { NatsComponent } from '../nats/NatsComponent.mjs';
 declare class RpcRequest<T1, T2> {
-    serverId: number;
+    serverId: string;
     serverType: string;
     rpcProto: string;
     reqType: Constructor<T1> | undefined;
@@ -10,10 +10,10 @@ declare class RpcRequest<T1, T2> {
     nats: NatsComponent | undefined;
     /**
      * 向哪个服务器发送
-     * @param svId 服务器id
+     * @param svUuid 服务器uuid
      * @returns
      */
-    to(svId: number): this;
+    to(svUuid: string): this;
     request(requestData: unknown): Promise<T2>;
 }
 export declare class RpcClient extends Component {
--- dist/src/rpc/RpcClient.mjs
+++ dist/src/rpc/RpcClient.mjs
@@ -1,7 +1,7 @@
 import { Component } from '../component/Component.mjs';
 import { NatsComponent } from '../nats/NatsComponent.mjs';
 class RpcRequest {
-    serverId = 0;
+    serverId = '';
     serverType = '';
     rpcProto = '';
     reqType;
@@ -9,11 +9,11 @@ class RpcRequest {
     nats;
     /**
      * 向哪个服务器发送
-     * @param svId 服务器id
+     * @param svUuid 服务器uuid
      * @returns
      */
-    to(svId) {
-        this.serverId = svId;
+    to(svUuid) {
+        this.serverId = svUuid;
         return this;
     }
     async request(requestData) {
@@ -25,6 +25,12 @@ class RpcRequest {
         const bytes = reqType.encode(reqType.create(requestData)).finish();
         // rpc.servertype/serverid.rpcProto
         // example: rpc.gate.Greeter.SayHello
+        if (!this.rpcProto)
+            throw new Error('rpcProto is required');
+        if (this.serverType && this.serverId)
+            throw new Error('serverType and serverId cannot be set at the same time');
+        if (!this.serverType && !this.serverId)
+            throw new Error('serverType or serverId must be set');
         const subject = `rpc.${this.serverId ? String(this.serverId) : this.serverType}.${this.rpcProto}`;
         const res = await this.nats?.tryRequest(subject, bytes, { timeout: 1000 });
         const resType = this.resType;
